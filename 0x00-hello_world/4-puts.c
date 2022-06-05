cdf9cfde4364c0cf5fb545c59e1aa04f35dff3bb
Kathy SATERA <kathysatera@gmail.com>
2022-06-05 15:47:30 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.>
 /*
- * main - main block
+ * main - puts
  * Return : 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
