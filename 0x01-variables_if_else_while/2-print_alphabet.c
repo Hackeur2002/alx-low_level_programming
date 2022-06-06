d032a0ddfd9d535bd9286cff848f8fe31fe7f45b
Kathy SATERA <kathysatera@gmail.com>
2022-06-06 15:10:54 -0700

    last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+/**
+ * * * main - Entry point
+ * * * Return: Always 0 (Success)
+ * **/
+
+int main(void)
+{
+	char n;
+	
+	for(n='a'; n < 'z'; n++)
+	{
+		putchar(n);
+		if (n = 'y')
+		{
+			putchar('z');
+		}
+	}
+	return (0);
+}
