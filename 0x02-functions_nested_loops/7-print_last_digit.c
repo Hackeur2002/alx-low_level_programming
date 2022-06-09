16f6e19d31c8365e5dfc1b52d58f4e0d6d5daa73
Kathy SATERA <kathysatera@gmail.com>
2022-06-09 02:36:56 -0700

    last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+ * print_last_digit-"The program gives the last digit of a number"
+ * @a: "The number to check"
+ * File: "6-abs.c"
+ * Return: "Always 0 (Success)
+ */
+
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
+
+
