6d1ac5fc0a4a10ac9a3f9dbe69d50294c8638081
Kathy SATERA <kathysatera@gmail.com>
2022-06-09 02:28:44 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * abs-"The program computes the absolute value of integer"
+ * @a: "The number to check"
+ * File: "6-abs.c"
+ * Return: "Always 0 (Success)
+ */
+
+int _abs(int a)
+{
+	return (abs(a));
+}
+
+
