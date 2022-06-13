4ed0ef42919c27b605df2c27982ffe3f849029f0
Kathy SATERA <kathysatera@gmail.com>
2022-06-13 16:45:39 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts-"This pogram reset the variable with pointers"
+ * @str: Le pointer
+ * Return: "Always 0 (Success)"
+ */
+
+void puts(char *str)
+{
+	puts(str);
+}
