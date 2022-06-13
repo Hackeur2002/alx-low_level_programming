a4410f9c1479c0afcde04090c6d7188b714c81d2
Kathy SATERA <kathysatera@gmail.com>
2022-06-13 15:39:30 -0700

    pointer-task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen-"This pogram reset the variable with pointers"
+ * @s: Le pointer
+ * return: lenght of a pointer
+ */
+
+void swap_int(char *s)
+{
+	return (strlen(*s));
+}
