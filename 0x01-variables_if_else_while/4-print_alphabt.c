c37594a8090423201a3414536c166f2e29e081f6
Kathy SATERA <kathysatera@gmail.com>
2022-06-15 16:18:51 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "math.h"
+/**
+ * * * main - Entry point
+ * * * Return: Always 0 (Success)
+ * **/
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z' && n != 'q' && n != 'e'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
