eceb36cc110b766affcf9a463f4e1fdef3898427
Kathy SATERA <kathysatera@gmail.com>
2022-06-16 14:33:16 -0700

    new

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,7 +5,7 @@
  * print_triangle-"this program print triangle"
  * @size: the numbers
  * File: 10-print_triangle
- * */
+ */
 
 void print_triangle(int size)
 {
@@ -13,17 +13,14 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (i = 0; i <= size; i++)
+		for (i = 1; i <= size; i++)
 		{
 			for (j = size - i; j > 0; j--)
 				_putchar(' ');
-			
 			for (j = 0; j < i; j++)
 				_putchar('#');
-
 			if (i == size)
 				continue;
-
 			_putchar('\n');
 		}
 	}
