db0742e60cf5d8fb3826f7a0c6581141aaf86fa4
Kathy SATERA <kathysatera@gmail.com>
2022-06-20 07:50:31 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp-"compare two chaine"
+ * @s1: first pointer
+ * @s2: second pointer
+ * Return: a nomber
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
