d0f659e1ae36bfada8672d330950522e65695fdf
Kathy SATERA <kathysatera@gmail.com>
2022-06-22 03:47:59 -0700

    toupper function

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * string_toupper-i"compare two chaine"
+ * @c: first pointe
+ * Return: lettters uppercase
+ */
+char *string_toupper(char  *c)
+{
+	return (toupper(c));
+}
