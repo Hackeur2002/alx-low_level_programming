3530475991b65edaff56121a3cac531057873028
Kathy SATERA <kathysatera@gmail.com>
2022-07-04 04:34:52 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr-cette fonction permet de rechercher une occurence
+ * @haystack: le tableau dans lequel on recherche
+ * @needle: ce qu'il faut rechercher
+ * Return: a pointer sur dest
+ */
+char *_strstr(char *haysatck, char *needle)
+{
+	char *retour;
+
+	retour = strstr(haystack, needle);
+	if (retour != NULL)
+	{
+		return (retour);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
