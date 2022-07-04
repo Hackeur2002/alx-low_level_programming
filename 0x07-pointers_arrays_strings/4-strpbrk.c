42043be414536be47950c2315036528cccccfbce
Kathy SATERA <kathysatera@gmail.com>
2022-07-03 17:15:59 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strpbrk-cette fonction permet de rechercher une occurence
+ * @s: le tableau dans lequel on recherche
+ * @accept: ce qu'il faut rechercher
+ * Return: a pointer sur dest
+ */
+char _strpbrk(char *s, char *accept)
+{
+	char retour;
+
+	retour = strpbrk(s, accept);
+	return (retour);
+}
