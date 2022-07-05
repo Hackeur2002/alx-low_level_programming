8dc2cea46065756473221996d70ead84e28f25a5
Kathy SATERA <kathysatera@gmail.com>
2022-07-04 17:25:20 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,11 +11,11 @@ void print_chessboard(char (*a)[8])
 	int i;
 	int j;
 
-	for (i = 0; i < a; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			printf("%c", [i][j]);
+			printf("%c", a[i][j]);
 		}
 		printf("\n");
 	}
