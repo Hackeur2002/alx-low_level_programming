0d719dbdd172a53503de3f457f10ee1703a493b6
Kathy SATERA <kathysatera@gmail.com>
2022-07-16 06:46:39 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the name
+ * @argc: number of argument
+ * @argv: un tablea
+ * Return: 0 Always
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
