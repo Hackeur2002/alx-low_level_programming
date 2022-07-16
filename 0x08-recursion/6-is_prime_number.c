026fe03cdd23ca354d88d231488fed5406d45c1e
Kathy SATERA <kathysatera@gmail.com>
2022-07-16 06:16:17 -0700

    update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,20 @@
 #include "main.h"
-
+/**
+ * _premier - donne le nombre
+ * @n:le nombre
+ * @x:un autre nombre
+ * Return: 1 ou 0
+ */
+int _premier(int n, int x)
+{
+	if (n % x == 0)
+	{
+		if (n == x)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + _premier(n, x + 1));
 /**
  * is_prime_number - print recursion string
  * @n:number
@@ -14,21 +29,4 @@ int is_prime_number(int n)
 	if (n == 1)
 		return (0);
 	return (_premier(n, 2));
-}
-/**
- * _premier - donne le nombre premier
- * @n:le nombre
- * @c:un autre nombre
- * Return: 1 ou 0
- */
-int _premier(int n, int c)
-{
-	if (n % c == 0)
-	{
-		if (n == c)
-			return (1);
-		else
-			return (0);
-	}
-	return (0 + _premier(n, c + 1));
 }
